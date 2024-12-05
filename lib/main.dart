fcf58097514d94961c4ab18025da9d027e336a25
julianskieee <alfithojls73@gmail.com>
2024-12-05 09:40:06 +0700

    pertemuan 9

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:aslab_travel/latihanmodul/pertemuan4.dart';
 import 'package:aslab_travel/latihanmodul/pertemuan5.dart';
 import 'package:aslab_travel/latihanmodul/pertemuan6.dart';
+import 'package:aslab_travel/latihanmodul/pertemuan9API.dart';
 import 'package:aslab_travel/project/screens/home_page.dart';
 import 'package:aslab_travel/project/screens/landing_page.dart';
 import 'package:aslab_travel/project/screens/splashscreen.dart';
@@ -16,7 +17,7 @@ class TravelApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      home: Pertemuan9APIScreen(),
     );
   }
 }
